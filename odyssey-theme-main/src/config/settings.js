10505adeac98b08f178b1353b44a4226563d462a
Mario "Boost" Bustillos <mario.bustillos@spaceforce.mil>
2024-05-20 09:24:58 -0600

    small updates

--- odyssey-theme-main/src/config/settings.js
+++ odyssey-theme-main/src/config/settings.js
@@ -3,6 +3,7 @@ export default {
 	description: `A simple, clean, and modern theme for a startup or businesses' marketing website.`,
 	url: `https://odyssey-theme.littlesticks.dev`, // No trailing slash!
 	name: `Myco`, // The short name of the business or brand name. Used for things like the copyright in the footer.
-	enableThemeSwitcher: true,
-	showLittleSticksPlug: true, // Disable this if you want to remove the Little Sticks plug from the footer. (╯°□°)╯︵ ┻━┻
+	enableThemeSwitcher: false,
+	contractorsLicense: '000000',
+	showlicense: true,
 };
