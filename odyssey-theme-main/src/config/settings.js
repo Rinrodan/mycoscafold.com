c6a305ea60635a8735d9ab2215a438771aa086b6
Mario "Boost" Bustillos <mario.bustillos@spaceforce.mil>
2024-06-17 05:29:51 -0600

    multiple updates

--- odyssey-theme-main/src/config/settings.js
+++ odyssey-theme-main/src/config/settings.js
@@ -1,9 +1,14 @@
 export default {
-	title: `MycoScaffold | Safe, Quality, Scaffold`,
-	description: `A simple, clean, and modern theme for a startup or businesses' marketing website.`,
-	url: `https://odyssey-theme.littlesticks.dev`, // No trailing slash!
+	description: `The scaffold builders scaffolding company`,
+	url: `https://mycoscaffod.com`, // No trailing slash!
 	name: `Myco`, // The short name of the business or brand name. Used for things like the copyright in the footer.
 	enableThemeSwitcher: false,
-	contractorsLicense: '000000',
+	email: "MycoScaffold@gmail.com",
+	phone: "(916) 538-1551",
+	contractorsLicense: '1121193',
+	showlicense: true,
+	mainFont: `https://fonts.googleapis.com/css2?family=Stick+No+Bills&display=swap`,
+	logo: "https://res.cloudinary.com/daa3cj6jz/image/upload/v1717725094/blue-atlas-5000x4000-white-bg_e4iagg.png",
+	
 	showlicense: true,
 };
